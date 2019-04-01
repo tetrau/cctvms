0673569e038cbb399da84cc1037742fd734f8625
tetrau <tetrau01@gmail.com>
2019-04-02 05:46:37 +0800

    Add alignment option
    Alignment the recording segment starting time and ending time to integral multiple of segemrnt length in order to make the recording filename timestamp looks more ordered.

--- cctvms.py
+++ cctvms.py
@@ -9,31 +9,39 @@ logger.addHandler(logging.NullHandler())
 
 
 class CCTVMS:
-    def __init__(self, rtsp, record_dir, segment=1800, prefix="record-", datetime_format="%Y-%m-%dT%H:%M"):
+    def __init__(self, rtsp, record_dir, segment=1800,
+                 prefix="record-", datetime_format="%Y-%m-%dT%H:%M", alignment=True):
         self.rtsp = rtsp
         self.record_dir = os.path.abspath(record_dir)
-        self.segment = segment
+        self.segment = int(segment)
         self.prefix = prefix
         self.datetime_format = datetime_format
+        self.alignment = alignment
 
     def record(self):
+        if self.alignment:
+            now = int(time.time())
+            duration = self.segment - (now % self.segment)
+        else:
+            duration = self.segment
+        filename = self.output_filename(duration)
+        logger.info("start recording rtsp stream to {}".format(filename))
         p = subprocess.run(
             ["vlc", self.rtsp,
-             "--sout=file/mp4:{}".format(os.path.join(self.record_dir, self.output_filename())),
+             "--sout=file/mp4:{}".format(os.path.join(self.record_dir, filename)),
              "-I", "dummy",
-             "--stop-time={}".format(int(self.segment)),
+             "--stop-time={}".format(duration),
              "vlc://quit"],
             stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        logger.info("end recording")
 
-    def output_filename(self):
+    def output_filename(self, duration):
         now = time.time()
-        end = now + self.segment
+        end = now + duration
         now = datetime.datetime.fromtimestamp(now).strftime(self.datetime_format)
         end = datetime.datetime.fromtimestamp(end).strftime(self.datetime_format)
         return "{}{}_{}.mp4".format(self.prefix, now, end)
 
     def run(self):
         while True:
-            logger.info("start recording")
             self.record()
-            logging.info("end recording")
